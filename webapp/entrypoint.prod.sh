aa40534f72ee598e03234f0ead167857f6948703
Marcel Tode <marcel@algorithmalley.nl>
2025-05-16 02:51:10 +0200

    Try deploy to koyeb

--- webapp/entrypoint.prod.sh
+++ webapp/entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --noinput
 python manage.py createsuperuser --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 project.wsgi:application
+python -m gunicorn --bind :8000 --workers 3 project.wsgi:application
